22d90ba6dca7734beb4bc2b017c8d9906d727d83
jjmonte20 <jjmonte20@gmail.com>
2019-06-19 21:01:40 -0500

    added the js to get the correct time

--- app.js
+++ app.js
@@ -1,9 +1,23 @@
 const secondHand = document.querySelector('.second-hand');
+const minuteHand = document.querySelector('.min-hand');
+const hourHand = document.querySelector('.hour-hand');
 function setDate() {
     const now =  new Date();
+
+    // seconds
     const seconds = now.getSeconds();
     const secondsDegrees = ((seconds / 60) * 360);
     secondHand.style.transform = `rotate(${secondsDegrees}deg)`;
+
+    // minutes
+    const mins = now.getMinutes();
+    const minsDegrees = ((mins / 60) * 360) + 90;
+    minuteHand.style.transform = `rotate(${minsDegrees}deg)`;
+
+    // hours
+    const hours = now.getHours();
+    const hourDegrees = ((hours / 12) * 360) + 90;
+    minuteHand.style.transform = `rotate(${hourDegrees}deg)`;
 }
 
 setInterval(setDate, 1000);
