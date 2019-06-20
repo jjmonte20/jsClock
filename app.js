18bcd11101bd192d272b9bd17591ed7d223b88bf
jjmonte20 <jjmonte20@gmail.com>
2019-06-19 21:12:25 -0500

    added notes for future reference

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
+// event listeners
 const secondHand = document.querySelector('.second-hand');
 const minuteHand = document.querySelector('.min-hand');
 const hourHand = document.querySelector('.hour-hand');
+
 function setDate() {
     const now =  new Date();
 
